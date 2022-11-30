56fd8094744aa7850237435218cab8a336446bfb
lamlt153027 <105737052+lamlt153027@users.noreply.github.com>
2022-11-30 14:06:40 +0700

    hehe

--- angular/src/account/login/login.component.ts
+++ angular/src/account/login/login.component.ts
@@ -3,6 +3,7 @@ import { AbpSessionService } from 'abp-ng2-module';
 import { AppComponentBase } from '@shared/app-component-base';
 import { accountModuleAnimation } from '@shared/animations/routerTransition';
 import { AppAuthService } from '@shared/auth/app-auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   styleUrls: ['./login.component.css'],
@@ -35,5 +36,40 @@ export class LoginComponent extends AppComponentBase {
   login(): void {
     this.submitting = true;
     this.authService.authenticate(() => (this.submitting = false));
+    
   }
+
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required,),
+    password: new FormControl('', Validators.required)
+  });
+  get email() { return this.loginForm.get('email'); }
+  
+  get password() { return this.loginForm.get('password'); }
+  checkClick = false;
+  CheckClick(){
+    this.checkClick = true;
+  }
+  checkClickOutside = false;
+  CheckClickOutside(){
+    if(this.checkClick == true){
+      this.checkClickOutside = true;
+    }
+  }
+
+  checkClick1 = false;
+  CheckClick1(){
+    this.checkClick1 = true;
+  }
+  checkClickOutside1 = false;
+  CheckClickOutside1(){
+    if(this.checkClick1 == true){
+      this.checkClickOutside1 = true;
+    }
+  }
+  checkClick2 = false;
+  CheckClick2(){
+    this.checkClick2 = true;
+  }
+
 }
